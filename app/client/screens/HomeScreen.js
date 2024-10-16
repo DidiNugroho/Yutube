23ec18c1772aead6056e8438253d6e695cf505ef
RenFrost <diidnugroho@gmail.com>
2024-10-16 17:10:51 +0700

    chore: initial react-native setup, feat: adding login screen/home screen and stack navigator

---
+++ app/client/screens/HomeScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+export default function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to the Home Screen!</Text>
+      <Button title="Logout" onPress={() => navigation.replace('Login')} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+});
---
+++ app/client/screens/LoginScreen.js
@@ -0,0 +1,40 @@
+// screens/LoginScreen.js
+import { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+
+export default function LoginScreen({ navigation }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    
+    navigation.replace('Home'); 
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={{marginBottom: 20, alignItems: 'center'}}>
+        <Text>Login</Text>
+      </View>
+      <TextInput
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+        style={styles.input}
+      />
+      <Button title="Login" onPress={handleLogin} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center', padding: 20 },
+  input: { height: 40, borderColor: 'gray', borderWidth: 1, marginBottom: 10, padding: 10 },
+});
