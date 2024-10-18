16139b25701b221972293cb0847d018cd7273d07
RenFrost <diidnugroho@gmail.com>
2024-10-18 20:01:01 +0700

    chore: adding cors

--- server/index.js
+++ server/index.js
@@ -1,33 +1,57 @@
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
 
-const {userTypeDefs, userResolvers} = require('./schema/user')
-const {postTypeDefs, postResolvers} = require('./schema/post')
-const {followTypeDefs, followResolvers} = require('./schema/follow');
+const { userTypeDefs, userResolvers } = require("./schema/user");
+const { postTypeDefs, postResolvers } = require("./schema/post");
+const { followTypeDefs, followResolvers } = require("./schema/follow");
 const { verifyToken } = require("./helpers/jwt");
 const UserModel = require("./models/userModel");
 
 const server = new ApolloServer({
-  typeDefs: [userTypeDefs, postTypeDefs, followTypeDefs], 
-  resolvers: [userResolvers, postResolvers, followResolvers], 
+  typeDefs: [userTypeDefs, postTypeDefs, followTypeDefs],
+  resolvers: [userResolvers, postResolvers, followResolvers],
+  introspection: true
 });
+
+const PORT = process.env.PORT || 3000;
 
 // Start the standalone server
 startStandaloneServer(server, {
-  listen: { port: 3000 },
+  listen: { port: PORT },
+
+  cors: {
+    origin: "*", 
+    methods: ["GET", "POST", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  },
+
   context: ({ req }) => {
     return {
       authentication: async () => {
         const authorization = req.headers.authorization;
-        if(!authorization) throw new Error("You have to login first!")
+        if (!authorization) throw new Error("You have to login first!");
 
-        const token = authorization.split(" ")[1];  
+        const token = authorization.split(" ")[1];
         const decoded = verifyToken(token);
-        const user = await UserModel.getUser(decoded._id)
+        const user = await UserModel.getUser(decoded._id);
         return user;
-      }
-    }
-  }
+      },
+    };
+  },
 }).then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
+
+  process.on("SIGINT", () => {
+    console.log("Shutting down the server...");
+    server.stop().then(() => {
+      process.exit(0);
+    });
+  });
+
+  process.on("SIGTERM", () => {
+    console.log("Shutting down the server...");
+    server.stop().then(() => {
+      process.exit(0);
+    });
+  });
 });
